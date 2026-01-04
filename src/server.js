e05c8c92f9f3be6ef4ab92aabac04f7aaec85d32
MariamSawera <mariamsawera@gmail.com>
2026-01-04 17:20:06 +0500

    backend finish

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ import express from 'express';
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from './config/db.js';
 import dotenv from "dotenv"
+import rateLimiter from './middleware/rateLimiter.js';
 
 dotenv.config();
 
@@ -9,15 +10,20 @@ const app = express();
 app.use(express.json()) //need this for reading body 
 const PORT = process.env.PORT || 5001
 
-connectDB();
 
 // middleware
 
+app.use(rateLimiter)
 app.use("/api/notes", notesRoutes); //instead of writting api/notes in all routes we using app.use ....by importing
 
 
+app.use((req, res, next) =>{
+  console.log(`the req method is ${req.method} and Req URL is ${req.url}` )
+  next();
+})
 
-
-app.listen(PORT, () => {
-  console.log('Server is running on port:', PORT);
+connectDB().then(() => {       //db connected first then applicated should start , small optimization
+  app.listen(PORT, () => {
+    console.log('Server is running on port:', PORT);
+  });
 });
